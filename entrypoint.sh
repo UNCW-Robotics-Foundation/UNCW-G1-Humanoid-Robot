b1cdae9e0412528114e612ebc38456193caadcd3
Art3mi0 <temomez123@gmail.com>
2026-07-02 14:25:49 -0400

    Small entrypoint change

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-#source /home/unitree_ros2/setup.sh && cd UNCW-G1-Robot/ && source ./install/local_setup.bash && ./install/g1_lower/bin/wireless_controller_test
-#source /home/unitree_ros2/setup.sh && cd UNCW-G1-Robot/ && source ./install/local_setup.bash && ros2 launch g1_lower g1_xbox_launch.xml
-source /home/unitree_ros2/setup.sh && cd UNCW-G1-Robot/ && source ./install/local_setup.bash && ros2 launch g1_lower g1_gestures_launch.xml
+#source /home/unitree_ros2/setup.sh && cd UNCW-G1-Humanoid-Robot/ && source ./install/local_setup.bash && ./install/g1_lower/bin/wireless_controller_test
+#source /home/unitree_ros2/setup.sh && cd UNCW-G1-Humanoid-Robot/ && source ./install/local_setup.bash && ros2 launch g1_lower g1_xbox_launch.xml
+source /home/unitree_ros2/setup.sh && cd UNCW-G1-Humanoid-Robot/ && source ./install/local_setup.bash && ros2 launch g1_lower g1_gestures_launch.xml
 
 exec "$@"
