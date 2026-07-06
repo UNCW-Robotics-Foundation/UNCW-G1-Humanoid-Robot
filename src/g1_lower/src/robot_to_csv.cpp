3657cb034e5a3a5eb8fa4306a8ffb03d31e975af
Art3mi0 <temomez123@gmail.com>
2026-07-06 16:18:23 -0400

    Updated CSV to include a flag for types of recordings

--- src/g1_lower/src/robot_to_csv.cpp
+++ src/g1_lower/src/robot_to_csv.cpp
@@ -18,17 +18,25 @@ class RobotToCSV : public rclcpp::Node {
                 });
 
             thread_ = std::thread([this]() { ControlLoop(); });
+
+            timer1_ = this->create_wall_timer(std::chrono::milliseconds(2),
+                                      [this] { ContinuousRec(); });
         }
 
     private:
         std::thread thread_;
         bool state_received = false;
-        bool stop_flag = false;
+        bool c_rec_flag = false;
         std::array<float, 29> g1JointPos{};
         std::vector<std::array<float, 29>> storedJoints;
+        std::vector<int> storedFlags;
 
         rclcpp::Subscription<unitree_hg::msg::LowState>::SharedPtr suber_;
         rclcpp::TimerBase::SharedPtr timer1_;
+
+        int counter = 0;
+        int max = 10;
+        std::string spacing = "         ";
 
         void LowStateHandler(unitree_hg::msg::LowState::SharedPtr message) {
             for (int i = 0; i < 29; i++) {
@@ -39,7 +47,6 @@ class RobotToCSV : public rclcpp::Node {
 
         void ControlLoop() {
             std::string str;
-            int counter = 0;
             while (!state_received) {
             RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 1000,
                                 "Waiting for LowState...");
@@ -48,20 +55,34 @@ class RobotToCSV : public rclcpp::Node {
             RCLCPP_INFO(this->get_logger(), "LowState subscribed. Recording starting...");
 
             std::cout << std::endl;
-            std::cout << "Press enter to record. Type anything before hitting enter to stop: ";
+            std::cout << "Empty input for single point, input u for user point, input c for continuous recording. input anything else to stop: ";
             std::getline(std::cin, str);
-            while (str == "") {
-                storedJoints.push_back(g1JointPos);
-                counter++;
-                if (counter < 10) {
-                    std::cout << counter << "  point recorded. (enter: record; anything+enter: exit): ";
-                } else {
-                    std::cout << counter << " point recorded. (enter: record; anything+enter: exit): ";
+            while ((str == "") || (str == "c") || (str == "u")) {
+                if (str == "") {
+                    storedFlags.push_back(0);
+                } else if (str == "u") {
+                    storedFlags.push_back(2);
                 }
+
+                if (str == "c") {
+                    c_rec_flag = true;
+                    std::cout << "Continuous recording started. Press enter to stop...";
+                    std::getline(std::cin, str);
+                    c_rec_flag = false;
+                } else{
+                    storedJoints.push_back(g1JointPos);
+                    counter++;
+                    if (counter >= max) {
+                        max = max * 10;
+                        spacing.pop_back();
+                }
+                }
+                std::cout << counter << spacing << "point(s) recorded. (empty: regular point; u: user point; c: continuous recording; anything else: exit): ";
                 std::getline(std::cin, str);
+
             }
             std::cout << std::endl;
-            RCLCPP_INFO(this->get_logger(), "Recording stopped");
+            RCLCPP_INFO(this->get_logger(), "Recording ended");
             if (counter > 0) {
                 FileWriter();
             }
@@ -72,12 +93,13 @@ class RobotToCSV : public rclcpp::Node {
             std::string tmp_line;
             std::ofstream CSVFile("csv_test.csv");
             RCLCPP_INFO(this->get_logger(), "Writing to csv...");
-            CSVFile << "LEFT_HIP_PITCH,LEFT_HIP_ROLL,LEFT_HIP_YAW,LEFT_KNEE,LEFT_ANKLE_PITCH,LEFT_ANKLE_ROLL,RIGHT_HIP_PITCH,RIGHT_HIP_ROLL,RIGHT_HIP_YAW,RIGHT_KNEE,RIGHT_ANKLE_PITCH,RIGHT_ANKLE_ROLL,WAIST_YAW,WAIST_ROLL,WAIST_PITCH,LEFT_SHOULDER_PITCH,LEFT_SHOULDER_ROLL,LEFT_SHOULDER_YAW,LEFT_ELBOW,LEFT_WRIST_ROLL,LEFT_WRIST_PITCH,LEFT_WRIST_YAW,RIGHT_SHOULDER_PITCH,RIGHT_SHOULDER_ROLL,RIGHT_SHOULDER_YAW,RIGHT_ELBOW,RIGHT_WRIST_ROLL,RIGHT_WRIST_PITCH,RIGHT_WRIST_YAW\n";
+            CSVFile << "LEFT_HIP_PITCH,LEFT_HIP_ROLL,LEFT_HIP_YAW,LEFT_KNEE,LEFT_ANKLE_PITCH,LEFT_ANKLE_ROLL,RIGHT_HIP_PITCH,RIGHT_HIP_ROLL,RIGHT_HIP_YAW,RIGHT_KNEE,RIGHT_ANKLE_PITCH,RIGHT_ANKLE_ROLL,WAIST_YAW,WAIST_ROLL,WAIST_PITCH,LEFT_SHOULDER_PITCH,LEFT_SHOULDER_ROLL,LEFT_SHOULDER_YAW,LEFT_ELBOW,LEFT_WRIST_ROLL,LEFT_WRIST_PITCH,LEFT_WRIST_YAW,RIGHT_SHOULDER_PITCH,RIGHT_SHOULDER_ROLL,RIGHT_SHOULDER_YAW,RIGHT_ELBOW,RIGHT_WRIST_ROLL,RIGHT_WRIST_PITCH,RIGHT_WRIST_YAW,FLAG\n";
             for (int i = 0; i < storedJoints.size(); i++) {
                 tmp_line = std::to_string(storedJoints[i][0]);
                 for (int j = 1; j < 29; j++) {
                     tmp_line += "," + std::to_string(storedJoints[i][j]);
                 }
+                tmp_line += "," + std::to_string(storedFlags[i]);
                 if (i < storedJoints.size() - 1) {
                    tmp_line += "\n"; 
                 }
@@ -85,6 +107,18 @@ class RobotToCSV : public rclcpp::Node {
             }
             CSVFile.close();
             RCLCPP_INFO(this->get_logger(), "Writing complete.");
+        }
+
+        void ContinuousRec() {
+            if (c_rec_flag) {
+                storedJoints.push_back(g1JointPos);
+                storedFlags.push_back(1);
+                counter ++;
+                if (counter >= max) {
+                    max = max * 10;
+                    spacing.pop_back();
+                }
+            }
         }
 
 };
