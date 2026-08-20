9f05e0c89ab7c99e25a53871d7d0e439a8ae001f
Art3mi0 <temomez123@gmail.com>
2026-08-20 17:41:37 -0400

    Cleaned up tele_arms code

--- src/g1_lower/src/g1_tele_arms.cpp
+++ src/g1_lower/src/g1_tele_arms.cpp
@@ -37,10 +37,7 @@ std::array<G1Arm7JointIndex, NUM_ARM_JOINTS> arm_joints_ = {
     G1Arm7JointIndex::RIGHT_WRIST_ROLL,
     G1Arm7JointIndex::RIGHT_WRIST_PITCH,
     G1Arm7JointIndex::RIGHT_WRIST_YAW};
-std::array<float, NUM_ARM_JOINTS> target_pos_ = {
-    0.0F, PI_2,  0.0F, PI_2, 0.0F, 0.0F, 0.0F,  // left
-    0.0F, -PI_2, 0.0F, PI_2, 0.0F, 0.0F, 0.0F   // right
-};
+
 // Stiffness for all G1 Joints
 const std::array<float, 29> Kp{
     60, 60, 60, 100, 40, 40,      // legs
@@ -64,7 +61,6 @@ const std::array<float, 29> Kd{
     // ROS2接口初始化
     //cmd_pub_ = this->create_publisher<LowCmd>("/arm_sdk", 10);
     cmd_pub_ = this->create_publisher<LowCmd>("/lowcmd", 10);
-    rviz_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
     arm_joints_pub_ = this->create_publisher<g1_msgs::msg::ArmStates>("/arm_joints", 10);
     lowstate_sub_ = this->create_subscription<LowState>(
         "/lowstate", 10,
@@ -72,17 +68,11 @@ const std::array<float, 29> Kd{
     ik_sub_ = this->create_subscription<g1_msgs::msg::ArmStates>(
         "/ik_sol", 10,
         [this](const g1_msgs::msg::ArmStates::SharedPtr msg) { IkCallback(msg); });
-    js_sub_ = this->create_subscription<sensor_msgs::msg::JointState>(
-        "/joint_states", 10,
-        [this](const sensor_msgs::msg::JointState::SharedPtr msg) { JSCallback(msg); });
 
     sleep_time_ =
         std::chrono::milliseconds(static_cast<int>(control_dt_ * 1000));
 
     thread_ = std::thread([this]() { InitRobot(); });
-
-    timer1_ = this->create_wall_timer(std::chrono::milliseconds(2),
-                                      [this] { FrameBroadcast(); });
 
     tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
     joy_suber_ = this->create_subscription<sensor_msgs::msg::Joy>(
@@ -101,8 +91,6 @@ const std::array<float, 29> Kd{
   rclcpp::TimerBase::SharedPtr timer1_;
   std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_suber_; 
-  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr rviz_pub_;
-  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr js_sub_; 
 
   LowState last_state_;
   g1_msgs::msg::ArmStates ik_sol;
@@ -118,8 +106,6 @@ const std::array<float, 29> Kd{
 
   std::array<g1_msgs::msg::MotorState, NUM_ARM_JOINTS> current_arm_pos_{};
   g1_msgs::msg::ArmStates current_arms;
-  sensor_msgs::msg::JointState initial_js;
-  sensor_msgs::msg::JointState new_js;
 
   const float kp_high = 300.0;
   const float kd_high = 3.0;
@@ -132,29 +118,13 @@ const std::array<float, 29> Kd{
   float t_y = 0.0;
   float t_z = 0.0;
   bool btn_flag = false;
-  bool once_flag = true;
-  bool js_once_flag = true;
+  bool init_flag = false;
+  bool busy_flag = false;
 
   float move_duration_ = 3.0F;
 
   void StateCallback(const LowState::SharedPtr msg) {
     last_state_ = *msg;
-
-    // if (state_received_) {
-    //   return;
-    // }
-    // if (once_flag) {
-    //   for (size_t i = 0; i < arm_joints_.size(); ++i) {
-    //     g1_msgs::msg::MotorState tmp_joint;
-    //     tmp_joint.q = last_state_.motor_state[static_cast<int>(arm_joints_[i])].q;
-    //     tmp_joint.dq = last_state_.motor_state[static_cast<int>(arm_joints_[i])].dq;
-    //     current_arm_pos_[i] = tmp_joint;
-    //   }
-    //   current_arms.motor_states = current_arm_pos_;
-    //   once_flag = false;
-    // } else {
-    //   arm_joints_pub_->publish(current_arms);
-    // }
 
     g1_msgs::msg::ArmStates current_arms;
     for (size_t i = 0; i < arm_joints_.size(); ++i) {
@@ -175,12 +145,7 @@ const std::array<float, 29> Kd{
     LowCmd cmd;
     //cmd.mode_pr = 0;
     //cmd.mode_machine = 5;
-    // new_js.position = initial_js.position;
-    // new_js.name = initial_js.name;
 
-    // if (state_received_) {
-    //   return;
-    // }
     for (int i = 15; i < 29; ++i) {
       cmd.motor_cmd[i].q = ik_sol.motor_states[i-15].q;
       cmd.motor_cmd[i].dq = 0.0F;
@@ -188,51 +153,14 @@ const std::array<float, 29> Kd{
       //cmd.motor_cmd[i].mode = 1;
       cmd.motor_cmd[i].kp = Kp[i];
       cmd.motor_cmd[i].kd = Kd[i];
-      // if (i - 15 < 7) {
-      //   new_js.position[i] = ik_sol.motor_states[i-15].q;
-      // } else {
-      //   new_js.position[i + 16] = ik_sol.motor_states[i-15].q;
-      // }
     }
     cmd.motor_cmd[29].q = 1.0F;
-    //new_js.header.stamp = this->get_clock()->now();
 
-    cmd_pub_->publish(cmd);
-    //rviz_pub_->publish(new_js);
-
-  }
-
-  void JSCallback(const sensor_msgs::msg::JointState::SharedPtr msg) {
-    if (js_once_flag) {
-      initial_js = *msg;
-      new_js = initial_js;
-      js_once_flag = false;
-      RCLCPP_INFO(this->get_logger(), "Initial joint_state saved");
-    } 
-    
-
-  }
-
-  void FrameBroadcast() {
-    // geometry_msgs::msg::TransformStamped t;
-
-    // t.header.stamp = this->get_clock()->now();
-    // t.header.frame_id = "world";
-    // t.child_frame_id = "target";
-    // t.transform.translation.x = 0.0 + t_x;
-    // t.transform.translation.y = 0.0 + t_y;
-    // t.transform.translation.z = 0.0 + t_z;
-    // t.transform.rotation.x = 0.0;
-    // t.transform.rotation.y = 0.0;
-    // t.transform.rotation.z = 0.0;
-    // t.transform.rotation.w = 1.0;
-
-    // tf_broadcaster_->sendTransform(t);
-
-    if (!js_once_flag) {
-      new_js.header.stamp = this->get_clock()->now();
-      // rviz_pub_->publish(new_js);
+    if (!busy_flag) {
+      cmd_pub_->publish(cmd);
     }
+    //cmd_pub_->publish(cmd);
+
   }
 
   void JoyHandler(sensor_msgs::msg::Joy::SharedPtr message) {
@@ -280,79 +208,44 @@ const std::array<float, 29> Kd{
       btn_flag = true;
     }
     else if (message->buttons[1] == 1) {   // B
-      // LowCmd cmd;
-      // for (int i = 0; i < 29; ++i) {
-      //   cmd.motor_cmd[i].q = 0.0F;
-      //   cmd.motor_cmd[i].dq = 0.0F;
-      //   cmd.motor_cmd[i].tau = 0.0F;
-      //   cmd.motor_cmd[i].kp = Kp[i];
-      //   cmd.motor_cmd[i].kd = Kd[i];
-      //   cmd.motor_cmd[i].mode = 1;
+      // std::array<float, NUM_ARM_JOINTS> current_lowstate_arms;
+      // for (int i = 15; i < 29; i++) {
+      //   current_lowstate_arms[i-15] = last_state_.motor_state[i].q;
       // }
-      // cmd.motor_cmd[29].q = 1.0F;
-
-      // cmd_pub_->publish(cmd);
-      std::array<float, NUM_ARM_JOINTS> current_lowstate_arms;
-      for (int i = 15; i < 29; i++) {
-        current_lowstate_arms[i-15] = last_state_.motor_state[i].q;
-      }
-      MoveToFinal({0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, current_lowstate_arms, 3.0F);
-      RCLCPP_INFO(this->get_logger(), "Robot Initialized");
-
-      //sensor_msgs::msg::JointState new_js;
-      new_js.position = initial_js.position;
-      new_js.name = initial_js.name;
-      //new_js.header.stamp = this->get_clock()->now();
-      //rviz_pub_->publish(new_js);
+      // MoveToInitial({0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, current_lowstate_arms, 3.0F);
+      // RCLCPP_INFO(this->get_logger(), "Robot Initialized");
+      init_flag = true;
+      busy_flag = true;
 
       btn_flag = true;
     }
-    else {
-        //RCLCPP_INFO(this->get_logger(), "CONTROLLER HANDLER; Listening...");
-    }
+
   }
 
   void InitRobot() {
     std::this_thread::sleep_for(sleep_time_);
-    // while (true) {
-    //   LowCmd cmd;
-    //   for (int i = 0; i < 29; ++i) {
-    //     cmd.motor_cmd[i].q = 0.0F;
-    //     cmd.motor_cmd[i].dq = 0.0F;
-    //     cmd.motor_cmd[i].tau = 0.0F;
-    //     cmd.motor_cmd[i].kp = Kp[i];
-    //     cmd.motor_cmd[i].kd = Kd[i];
-    //     cmd.motor_cmd[i].mode = 1;
-    //   }
-    //   cmd.motor_cmd[29].q = 1.0F;
-
-    //   cmd_pub_->publish(cmd);
-    //   //RCLCPP_INFO(this->get_logger(), "Robot Initialized");
-    // }
-    // LowCmd cmd;
-    // for (int i = 0; i < 29; ++i) {
-    //   cmd.motor_cmd[i].q = 0.0F;
-    //   cmd.motor_cmd[i].dq = 0.0F;
-    //   cmd.motor_cmd[i].tau = 0.0F;
-    //   cmd.motor_cmd[i].kp = Kp[i];
-    //   cmd.motor_cmd[i].kd = Kd[i];
-    //   cmd.motor_cmd[i].mode = 1;
-    // }
-    // cmd.motor_cmd[29].q = 1.0F;
-
-    // cmd_pub_->publish(cmd);
 
     std::array<float, NUM_ARM_JOINTS> current_lowstate_arms;
     for (int i = 15; i < 29; i++) {
       current_lowstate_arms[i-15] = last_state_.motor_state[i].q;
     }
-    MoveToFinal({0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, current_lowstate_arms, 3.0F);
-
+    MoveToInitial({0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, current_lowstate_arms, 3.0F);
     RCLCPP_INFO(this->get_logger(), "Robot Initialized");
+
+    while (true) {
+      if (init_flag) {
+        for (int i = 15; i < 29; i++) {
+          current_lowstate_arms[i-15] = last_state_.motor_state[i].q;
+        }
+        MoveToInitial({0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, current_lowstate_arms, 3.0F);
+        init_flag = false;
+        busy_flag = false;
+        RCLCPP_INFO(this->get_logger(), "Robot Initialized");
+      }
+    }
   }
 
-  // Same as function above without e_stop or last_move flags
-  void MoveToFinal(const std::array<float, NUM_ARM_JOINTS>& target,
+  void MoveToInitial(const std::array<float, NUM_ARM_JOINTS>& target,
               std::array<float, NUM_ARM_JOINTS>& current, float duration) {
     const int steps = static_cast<int>(duration / control_dt_);
     const std::array<float, NUM_ARM_JOINTS> initial = current;
